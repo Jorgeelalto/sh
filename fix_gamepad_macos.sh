607ab3a31ceb67b6de6e51a48f9f05e403b8bb7e
Jorge Marcos Chávez <jormarchav@gmail.com>
2023-03-18 11:34:24 +0100

    Some stuff

---
+++ fix_gamepad_macos.sh
@@ -0,0 +1,3 @@
+defaults write com.apple.GameController bluetoothPrefsMenuLongPressAction -integer 0
+defaults write com.apple.GameController bluetoothPrefsShareLongPressSystemGestureMode -integer -1
+
---
+++ nobass.sh
@@ -0,0 +1,1 @@
+demucs --two-stems=bass --mp3 --mp3-bitrate=320 "$1"
