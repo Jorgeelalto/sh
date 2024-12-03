55a684719dbc1d65ef3cf5c9729d491db532d60d
Jorge Marcos Chávez <jormarchav@gmail.com>
2024-12-03 20:27:19 +0100

    Make WAVtoFLAC good

--- WAVtoFLAC.sh
+++ WAVtoFLAC.sh
@@ -1,15 +1,9 @@
-if [ "$1" = "n" ]; then
-	echo "Keep original files mode"
-	find . -name '*.wav' | while read LINE; do flac -5 "$LINE"; done
-	exit 0
-fi
+for f in *.wav; do
+    flac -5 "$f";
+done
 
-if [ "$1" = "y" ]; then
-	echo "Remove files mode"
-	find . -name '*.wav' | while read LINE; do flac -5 "$LINE" && rm "$LINE"; done
-	exit 0
+echo "Do you want to remove the original .wav files? [y/n]"
+read -n 1 reply
+if [ $reply = "y" ]; then
+    rm *.wav
 fi
-
-echo "Invalid removal argument. Please specify whether to remove original WAV file. Usage:"
-echo "    ./WAVtoFLAC.sh [y/n]"
-exit 1
