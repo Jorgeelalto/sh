d6064bd2e5d0c070ef5a4042a6adfe548d3ef8e2
Jorge Marcos Chávez <jormarchav@gmail.com>
2026-01-01 22:30:28 +0100

    Fixes to youtube scripts

--- YTtoMP3.sh
+++ YTtoMP3.sh
@@ -1,1 +1,4 @@
+source ~/Venvs/yt-dlp/bin/activate
 yt-dlp -x --audio-format mp3 --prefer-ffmpeg "$1" --audio-quality 320K
+deactivate
+
--- YTtoMP4.sh
+++ YTtoMP4.sh
@@ -1,1 +1,4 @@
-youtube-dl -f 'bestvideo[height<=1080]+bestaudio/best[height<=1080]' $1
+source ~/Venvs/yt-dlp/bin/activate
+yt-dlp -f 'bestvideo[height<=1080]+bestaudio/best[height<=1080]' $1
+deactivate
+
